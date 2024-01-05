238a076e10e2f435bc4490cbda1e580463e8e40c
Tommy Zhang <tommyzhang@Tommys-MacBook-Air.local>
2024-01-05 14:10:41 -0800

    set up html & Javascript for practice

---
+++ javascript.js
@@ -0,0 +1,25 @@
+
+const name = "Tommy";
+const greeting = `hello, ${name}`;
+console.log(greeting);
+
+let text = "HELLOOOO WORLD";
+let char = text.charAt(6);
+let length = text.length;
+
+console.log(text, char, length);
+
+let letter = name[2];
+
+// var text = "HELLO WORLD";
+// document.getElementById("demo").innerHTML = text.charAt(0);
+
+// const button = document.querySelector("button");
+
+// function greet() {
+//     const name = prompt("what is your name?");
+//     const greeting = document.querySelector("#greeting");
+//     greeting.textContent = `Hello ${name}, nice to meet you!`;
+// }
+
+// button.addEventListener("click", greet);
