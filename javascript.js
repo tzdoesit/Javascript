3c6058673412e18f7c5d6fe8980f09ecf772f124
Tommy Zhang <tommy.n.zhang@gmail.com>
2024-04-06 21:15:21 -0700

    added phonebook code - not working however

--- javascript.js
+++ javascript.js
@@ -1,23 +1,25 @@
 
 
-const cats = ["Pete", "Biggles", "Jasmine"];
 
-let myFavoriteCats = "My cats are called ";
+
+// const cats = ["Pete", "Biggles", "Jasmine"];
+
+// let myFavoriteCats = "My cats are called ";
 
 // for (const cat of cats) {
 //     myFavoriteCats += `${cat}, `;
 // }
 
-for (let i = 0; i < cats.length; i++) {
-    if (i === cats.length - 1) {
-        myFavoriteCats += `and ${cats[i]}.`;
-    }
-    else {
-        myFavoriteCats += `${cats[i]}, `;
-    }
-}
+// for (let i = 0; i < cats.length; i++) {
+//     if (i === cats.length - 1) {
+//         myFavoriteCats += `and ${cats[i]}.`;
+//     }
+//     else {
+//         myFavoriteCats += `${cats[i]}, `;
+//     }
+// }
 
-console.log(myFavoriteCats);
+// console.log(myFavoriteCats);
 
 // you can nest arrays inside of an array, this is used somewhat often
 // let a = [
--- phonebookScript.js
+++ phonebookScript.js
@@ -10,24 +10,6 @@ const para = document.querySelector("p");
 const input = document.querySelector("input");
 const btn = document.querySelector("button");
 
-// btn.addEventListener("click", () => {
-//     const searchName = input.value.toLowerCase();
-//     input.value = "";
-//     input.focus();
-//     para.textContent = "";
-
-//     for (const contact of contacts) {
-//         const splitContact = contact.split(":");
-//         if (splitContact[0].toLowerCase() === searchName) {
-//             para.textContent = `${splitContact[0]}'s number is ${splitContact[1]}.`;
-//             break;
-//         }
-//     }
-//     if  (para.textContent === "") {
-//         para.textContent = "Contact not Found.";
-//     }
-// });
-
 btn.addEventListener("click", () => {
     const searchName = input.value.toLowerCase();
     input.value = "";
