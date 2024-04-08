2d1ba9c22cb044f52758ec0e73e6cac39ff682d6
Tommy Zhang <tommy.n.zhang@gmail.com>
2024-04-08 14:14:19 -0700

    countdown 10 -> 1 code added

--- javascript.js
+++ javascript.js
@@ -1,5 +1,24 @@
 
+const output = document.querySelector('.output');
+output.innerHTML = '';
 
+for (let i = 10; i >= 0; i--) {
+    const para = document.createElement('p');
+
+    if (i === 10){
+    para.textContent = `Countdown ${i}`;
+    }
+
+    else if (i === 0) {
+        para.textContent = `Blast off!`;
+    }
+
+    else {
+        para.textContent =  i;
+    }
+
+    output.appendChild(para);
+}
 
 
 // const cats = ["Pete", "Biggles", "Jasmine"];
